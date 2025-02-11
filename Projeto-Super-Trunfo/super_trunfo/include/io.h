65d9c54eba1a57d90b6e1a06cd9d9558cdd8aac8
JoaoPrissao <164431824+JoaoPrissao@users.noreply.github.com>
2025-02-11 00:28:35 -0300

    Delete Projeto-Super-Trunfo/super_trunfo directory

--- Projeto-Super-Trunfo/super_trunfo/include/io.h
+++
@@ -1,8 +0,0 @@
-#ifndef IO_H
-#define IO_H
-
-void exportarParaCSV();
-void salvarDeck();
-void carregarDeck();
-
-#endif
--- Projeto-Super-Trunfo/super_trunfo/include/jogo.h
+++
@@ -1,8 +0,0 @@
-#ifndef JOGO_H
-#define JOGO_H
-
-#include "cartas.h"
-
-void jogarPartida(Carta deck[], int totalCartas);
-
-#endif
